6945b86beaf99e68a82a72faefdd672e2357b85f
gabyfsy <gabrielastanton@gmail.com>
2021-07-12 16:37:18 -0400

    ejercicios arreglos

---
+++ ruby_exercises/arreglos.rb
@@ -0,0 +1,22 @@
+#acá irán cosas que se pueden hacer con arreglos
+a = ["Simba", "Colorada", "Bataraz", "Blanquita", "Perla"]
+b = [4, 2, 8, 10, 7, 1, 5, 9, 3, 6]
+c = ["Perla", 7]
+
+#puts a[0]
+#z=a+b
+#puts z.to_s
+#puts (a+b)-c
+#puts c.class
+#puts a.shuffle
+#se puede unir los elementos, separados con el caracter que está entre las comillas.
+#puts a.shuffle.join(", ")
+#puts a.fetch(0)
+#puts a.at(0)
+#a.delete("Colorada")
+#puts a.values_at(1,2).join(' y ')
+#puts b.sort
+#puts b.reverse
+#puts a.slice(2..4)
+a.insert(2,"Cazuela")
+puts a
